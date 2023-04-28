ef60872145f7a4e2b36900be9f136e52c0528aab
SamirAbbasov17 <abbasovsamir718@gmail.com>
2023-04-28 13:39:39 +0400

    tapsiriq tamamlandi

---
+++ Project/Project/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Project
+{
+    class Student : Person
+    {
+        Student(string name, string surName, int age, int iqRank, int languageRank)
+        {
+
+            Name = name;
+            SurName = surName;
+            Age = age;
+            IQRank = iqRank;
+            LanguageRank = languageRank;
+        }
+
+        private int iqRank;
+        private int languageRank;
+        private int age;
+
+        public override int Age { get { return age; } set { if (value > 20 || value < 6) { throw new Exception("Telebenin yasi uygun deyl"); } else { age = value; } } }
+        public int IQRank { get { return iqRank; } set { if (value >= 100) { iqRank = 100; } else { iqRank = value; } } }
+        public int LanguageRank { get { return languageRank; } set { if (value >= 100) { languageRank = 100; } else { languageRank = value; } } }
+
+        public string ExamResult()
+        {
+            if (iqRank + languageRank < 120)
+            {
+                return "Sinifde qaldin";
+            }
+            return (iqRank + languageRank).ToString();
+        }
+    }
+}
